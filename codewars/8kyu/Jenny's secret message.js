1a88ddf4b40fb6fe7949baa84efe2ee1a012e194
ViciousCodes <hamzazpatel@gmail.com>
2023-08-25 17:31:08 -0500

    solution

--- codewars/8kyu/Jenny's secret message.js
+++ codewars/8kyu/Jenny's secret message.js
@@ -1,3 +1,35 @@
 Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
 
 Can you help her
+
+
+
+function greet(name){
+    return "Hello, " + name + "!";
+    if(name === "Johnny")
+      return "Hello, my love!";
+  }
+
+
+  my solution 
+  function greet(name){
+     if(name === "Johnny") { 
+        return "Hello, my love!";
+    } else {
+           return "Hello, " + `${name}` + "!";
+  
+    }
+  }
+      
+
+  solution 1 
+  function greet(name){
+    return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+  }
+
+  soltion 2 function greet(name){  
+    if(name === "Johnny")
+      return "Hello, my love!";
+    return "Hello, " + name + "!";
+  }
+  soltuion 3 let greet = name => `Hello, ${name == "Johnny" ? "my love" : name}!`;
